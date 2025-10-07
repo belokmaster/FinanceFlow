d624706f1b6b8949488c832ee8e3481349714101
belokmaster <sychev.nickolay1@gmail.com>
2025-10-07 19:23:24 +0300

    add transfer, create new acc

--- db.go
+++ db.go
@@ -31,6 +31,7 @@ type TypeTransaction int
 const (
 	Income TypeTransaction = iota
 	Expense
+	Transfer
 )
 
 type Transaction struct {
@@ -47,14 +48,93 @@ type Transaction struct {
 	Date          time.Time
 }
 
+type TransferTransaction struct {
+	ID                uint `gorm:"primaryKey;autoIncrement"`
+	AccountID         uint
+	Account           Account
+	TransferAccountID uint
+	TransferAccount   Account
+	Type              TypeTransaction
+	Amount            float64
+	Comment           string
+	Date              time.Time
+}
+
 func InitDB(db *gorm.DB) error {
-	err := db.AutoMigrate(&Account{}, &Category{}, &SubCategory{}, &Transaction{})
+	err := db.AutoMigrate(&Account{}, &Category{}, &SubCategory{}, &Transaction{}, &TransferTransaction{})
 	if err != nil {
 		log.Fatalf("problem to create gorm bd: %v", err)
 	}
 
 	log.Println("The database created successfully")
 	return nil
+}
+
+func CreateNewAccount(db *gorm.DB, acc Account) error {
+	if acc.Name == "" {
+		return fmt.Errorf("empty name of account")
+	}
+
+	result := db.Create(&acc)
+	if result.Error != nil {
+		return fmt.Errorf("problem to create a new acc: %v", result.Error)
+	}
+
+	return nil
+}
+
+func TransferToAnotherAcc(db *gorm.DB, tx TransferTransaction) error {
+	if tx.Type != Transfer {
+		return fmt.Errorf("problem with transaction types")
+	}
+
+	if tx.AccountID == tx.TransferAccountID {
+		return fmt.Errorf("transfer to yourself")
+	}
+
+	if tx.Amount < 0 {
+		return fmt.Errorf("transfer is negative")
+	}
+
+	return db.Transaction(func(txDB *gorm.DB) error {
+		var fromAccount Account
+		var toAccount Account
+
+		err := txDB.First(&fromAccount, tx.AccountID).Error
+		if err != nil {
+			return err
+		}
+
+		err = txDB.First(&toAccount, tx.TransferAccountID).Error
+		if err != nil {
+			return err
+		}
+
+		if fromAccount.Balance < tx.Amount {
+			return fmt.Errorf("balance less then transfer sum")
+		}
+
+		// block change balance each accounts
+		fromAccount.Balance -= tx.Amount
+		err = txDB.Save(&fromAccount).Error
+		if err != nil {
+			return err
+		}
+
+		toAccount.Balance += tx.Amount
+		err = txDB.Save(&toAccount).Error
+		if err != nil {
+			return err
+		}
+
+		// create the record after all checks upper
+		err = txDB.Create(&tx).Error
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
 }
 
 func AddTransaction(db *gorm.DB, tx Transaction) error {
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"gorm.io/driver/sqlite"
@@ -49,6 +50,106 @@ func main() {
 		http.ServeFile(w, r, "index.html")
 	})
 
+	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "invalid form", http.StatusBadRequest)
+			return
+		}
+
+		accountID, err := strconv.Atoi(r.FormValue("ID"))
+		if err != nil {
+			http.Error(w, "problem with id. use normal values", http.StatusBadRequest)
+			return
+		}
+
+		name := r.FormValue("Name")
+		name = strings.TrimSpace(name) // why it works????????
+
+		balance, err := strconv.ParseFloat(r.FormValue("Balance"), 64)
+		if err != nil {
+			http.Error(w, "problem with balance. use normal values", http.StatusBadRequest)
+			return
+		}
+
+		acc := Account{
+			ID:      uint(accountID),
+			Name:    name,
+			Balance: balance,
+		}
+
+		err = CreateNewAccount(db, acc)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("failed to create a new acc: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		log.Println("A new account was created sucessfully")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+
+	http.HandleFunc("/transfer", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "invalid form", http.StatusBadRequest)
+			return
+		}
+
+		accountID, err := strconv.Atoi(r.FormValue("AccountID"))
+		if err != nil {
+			http.Error(w, "problem with account_id. use normal values", http.StatusBadRequest)
+			return
+		}
+
+		transferAccountID, err := strconv.Atoi(r.FormValue("TransferAccountID"))
+		if err != nil {
+			http.Error(w, "problem with transferaccount_id. use normal values", http.StatusBadRequest)
+			return
+		}
+
+		transType, err := strconv.Atoi(r.FormValue("Type"))
+		if err != nil {
+			http.Error(w, "problem with transtype. use normal values", http.StatusBadRequest)
+			return
+		}
+
+		amount, err := strconv.ParseFloat(r.FormValue("Amount"), 64)
+		if err != nil {
+			http.Error(w, "problem with amount. use normal values", http.StatusBadRequest)
+			return
+		}
+
+		comment := r.FormValue("Comment")
+
+		tx := TransferTransaction{
+			AccountID:         uint(accountID),
+			TransferAccountID: uint(transferAccountID),
+			Type:              TypeTransaction(transType),
+			Amount:            amount,
+			Comment:           comment,
+			Date:              time.Now(),
+		}
+
+		err = TransferToAnotherAcc(db, tx)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("failed to transfer: %v", err), http.StatusInternalServerError)
+			return
+		}
+
+		log.Println("Transfer added and accounts balance updated")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+
 	// add trans in server
 	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
@@ -62,11 +163,36 @@ func main() {
 			return
 		}
 
-		accountID, _ := strconv.Atoi(r.FormValue("AccountID"))
-		categoryID, _ := strconv.Atoi(r.FormValue("CategoryID"))
-		subCategoryID, _ := strconv.Atoi(r.FormValue("SubCategoryID"))
-		transType, _ := strconv.Atoi(r.FormValue("Type"))
-		amount, _ := strconv.ParseFloat(r.FormValue("Amount"), 64)
+		accountID, err := strconv.Atoi(r.FormValue("AccountID"))
+		if err != nil {
+			http.Error(w, "problem with account_id. use normal values", http.StatusBadRequest)
+			return
+		}
+
+		categoryID, err := strconv.Atoi(r.FormValue("CategoryID"))
+		if err != nil {
+			http.Error(w, "problem with category_id. use normal values", http.StatusBadRequest)
+			return
+		}
+
+		subCategoryID, err := strconv.Atoi(r.FormValue("SubCategoryID"))
+		if err != nil {
+			http.Error(w, "problem with subcategory_id. use normal values", http.StatusBadRequest)
+			return
+		}
+
+		transType, err := strconv.Atoi(r.FormValue("Type"))
+		if err != nil {
+			http.Error(w, "problem with transtype. use normal values", http.StatusBadRequest)
+			return
+		}
+
+		amount, err := strconv.ParseFloat(r.FormValue("Amount"), 64)
+		if err != nil {
+			http.Error(w, "problem with amount. use normal values", http.StatusBadRequest)
+			return
+		}
+
 		comment := r.FormValue("Comment")
 
 		tx := Transaction{
