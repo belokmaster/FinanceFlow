2bb8de182435df8ed94569cd0fb83e760956f45f
belokmaster <sychev.nickolay1@gmail.com>
2025-10-08 18:37:32 +0300

    add new handlers

--- db.go
+++ db.go
@@ -83,6 +83,77 @@ func CreateNewAccount(db *gorm.DB, acc Account) error {
 	return nil
 }
 
+func DeleteAccount(db *gorm.DB, id int) error {
+	result := db.Delete(&Account{}, id)
+	if result.Error != nil {
+		return fmt.Errorf("problem with delete account in db: %v", result.Error)
+	}
+
+	if result.RowsAffected == 0 {
+		return fmt.Errorf("account with id %d not found", id)
+	}
+
+	return nil
+}
+
+func AddCategory(db *gorm.DB, cat Category) error {
+	if cat.Name == "" {
+		return fmt.Errorf("empty name of category")
+	}
+
+	result := db.Create(&cat)
+	if result.Error != nil {
+		return fmt.Errorf("problem to create a new category: %v", result.Error)
+	}
+
+	return nil
+}
+
+func DeleteCategory(db *gorm.DB, id int) error {
+	result := db.Delete(&Category{}, id)
+	if result.Error != nil {
+		return fmt.Errorf("problem with delete category in db: %v", result.Error)
+	}
+
+	if result.RowsAffected == 0 {
+		return fmt.Errorf("category with id %d not found", id)
+	}
+
+	return nil
+}
+
+func AddSubCategory(db *gorm.DB, subCat SubCategory) error {
+	if subCat.Name == "" {
+		return fmt.Errorf("empty name of category")
+	}
+
+	var cat Category
+	err := db.First(&cat, subCat.CategoryID).Error
+	if err != nil {
+		return fmt.Errorf("category with ID %d not found", subCat.CategoryID)
+	}
+
+	result := db.Create(&subCat)
+	if result.Error != nil {
+		return fmt.Errorf("problem to create a new sub_category: %v", result.Error)
+	}
+
+	return nil
+}
+
+func DeleteSubCategory(db *gorm.DB, id int) error {
+	result := db.Delete(&SubCategory{}, id)
+	if result.Error != nil {
+		return fmt.Errorf("problem with delete sub_category in db: %v", result.Error)
+	}
+
+	if result.RowsAffected == 0 {
+		return fmt.Errorf("sub_category with id %d not found", id)
+	}
+
+	return nil
+}
+
 func TransferToAnotherAcc(db *gorm.DB, tx TransferTransaction) error {
 	if tx.Type != Transfer {
 		return fmt.Errorf("problem with transaction types")
